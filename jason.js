e15aaec401f1a41166f1c50f7e3976fbb25a7246
predicador2023 <josebedoya2013@gmail.com>
2023-05-02 15:41:40 -0300

    commit scroll menu

--- jason.js
+++ jason.js
@@ -3,11 +3,14 @@
 const barraNav = document.querySelector("#barraNav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const body = document.querySelector("#body");
 
 abrir.addEventListener("click", () => {
     barraNav.classList.add("visible");
+    body.classList.add("no-scroll");
 })
 
 cerrar.addEventListener("click", () =>  {
     barraNav.classList.remove("visible");
+    body.classList.remove("no-scroll");
 })
