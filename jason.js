daa06ffaee69678d2dd76bb9d76d1e5d7628efb6
predicador2023 <josebedoya2013@gmail.com>
2023-05-31 15:46:16 -0300

    slider seccion acerca de

--- jason.js
+++ jason.js
@@ -1,5 +1,5 @@
 
-
+// barra de navegacion
 const barraNav = document.querySelector("#barraNav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
@@ -14,3 +14,56 @@ cerrar.addEventListener("click", () =>  {
     barraNav.classList.remove("visible");
     body.classList.remove("no-scroll");
 })
+
+// -----------------------------------------------------------
+// silder de imagenes en seccion acerca de
+
+    var imagenes = [ "imagenesSlider/cruzJesus.jpg" , 
+         "imagenesSlider/mesaBiblia.jpg" , 
+         "imagenesSlider/niñoBiblia.jpg", 
+          "imagenesSlider/tresCruz.jpg"];
+        document.Imagen.src = imagenes[0];
+
+        var contador= 0;
+
+    
+        var sliderDerecha = document.querySelector(".slider-Derecho");
+        var sliderIzquierda = document.querySelector(".slider-Izquierdo");
+         function moverDerecha ( ) {
+            contador ++ ;
+            if (contador > imagenes.length -1){
+                contador=0;
+            }
+            
+            document.Imagen.src = imagenes[contador];
+
+         }
+
+         function moverIzquierda ( ) {
+            contador -- ;
+            if (contador < 0){
+                contador=imagenes.length -1;
+            }
+            document.Imagen.src = imagenes[contador];
+
+        }
+            
+            
+            
+                // para que se actualice cada 2,5 segundos cada imagen
+                //Al momento de darle click antes de los 2,5 segundos se pueda actualizar 
+                
+                var Intervalo = setInterval( moverDerecha , 2500 );
+
+                sliderDerecha.addEventListener( "click" , function () {
+                clearInterval(Intervalo);
+                moverDerecha();
+                Intervalo = setInterval( moverDerecha , 2500 );
+                 } );
+
+                sliderIzquierda.addEventListener( "click" , function () {
+                clearInterval(Intervalo);
+                moverIzquierda();
+                Intervalo = setInterval( moverDerecha , 2500 ); 
+               });
+          
