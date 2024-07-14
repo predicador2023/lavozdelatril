67448729605972134e22130eaaff1406d78ef870
predicador2023 <josebedoya2013@gmail.com>
2024-07-14 05:34:28 -0300

    commit btn

--- jason.js
+++ jason.js
@@ -95,7 +95,7 @@ const observer = new IntersectionObserver (callback, options)
    url: "https://lavozdelatril.vercel.app/laLunaDePaita.html",
  }
 
- const btnComp = document.querySelector("div .btnComp > button");
+ const btnComp = document.querySelectorAll("div.btnComp > button");
  //  const resultPara = document.querySelector(".result");
 
 // Share must be triggered by "user activation"
