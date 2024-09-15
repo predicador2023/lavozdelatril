bef2916a2e58a54d08b9e1c49fed48cf5a2966cf
predicador2023 <josebedoya2013@gmail.com>
2024-09-15 09:13:35 -0300

    localStorage dark mode

--- jason.js
+++ jason.js
@@ -36,12 +36,43 @@ const observer = new IntersectionObserver (callback, options)
 // boton modo dark
   const swith = document.querySelector(".switch");
 
-  swith.addEventListener("click", e => {
+  // swith.addEventListener("click", e => {
+  //   swith.classList.toggle("active");
+  //   document.body.classList.toggle("active");
+   
+  // })
+
+// ---------------------------------------------------------
+
+  document.addEventListener("DOMContentLoaded", e => {
+    cargarDarkModeDesdeLocalStorage()
+    swith.addEventListener("click", toggleDarkMode)
+  })
+
+  function toggleDarkMode () {
     swith.classList.toggle("active");
     document.body.classList.toggle("active");
+    guardarDarkModeEnLocalStorage(swith.classList.contains("active")) 
+    }
+
+  function  guardarDarkModeEnLocalStorage(estado) {
+    localStorage.setItem("darkMode", estado)
+  }
+
+  function cargarDarkModeDesdeLocalStorage () {
+    const DarkModeGuardado = localStorage.getItem("darkMode") === "true" ;  
+   if (DarkModeGuardado) {
+    swith.classList.add("active");
+    document.body.classList.add("active");
+   }  
+  }
    
-  })
+
   
+       
+  
+
+
 // -----------------------------------------------------------
 // silder de imagenes en seccion acerca de
 
